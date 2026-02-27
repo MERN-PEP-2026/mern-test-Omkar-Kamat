33273c9867d0ae4c2baadfb9819b626ae13c7fb6
Omkar Kamat <147087874+Omkar-Kamat@users.noreply.github.com>
2026-02-27 12:28:00 +0530

    auth middleware

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { verifyToken } from "../utils/jwt.js";
+import User from "../models/user.model.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = verifyToken(token);
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.user = {
+      id: user._id,
+      role: user.role
+    };
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
---
+++ backend/validators/auth.schema.js
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const registerSchema = z.object({
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name cannot exceed 50 characters")
+    .trim(),
+
+  email: z
+    .string()
+    .email("Invalid email format")
+    .trim()
+    .toLowerCase(),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(100, "Password too long"),
+
+  role: z
+    .enum(["STUDENT", "INSTRUCTOR", "ADMIN"])
+    .optional()
+});
+
+export const loginSchema = z.object({
+  email: z
+    .email("Invalid email format")
+    .trim()
+    .toLowerCase(),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+});
