0b843350efb7f7f964d26ce05e89b82848d766a1
Omkar Kamat <147087874+Omkar-Kamat@users.noreply.github.com>
2026-02-27 12:24:12 +0530

    auth routes + app config

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,30 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
-app.use(cors());
-app.use(express.json({limit: "10mb"}));
-app.use(express.urlencoded({extended:true, limit: "10mb"}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
-app.get("/health",(req,res)=>{
-    res.send({
-        status: "ok"
-    })
-})
+app.get("/health", (req, res) => {
+  res.send({ status: "ok" });
+});
+
+app.use("/api/auth", authRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
 
 export default app;
