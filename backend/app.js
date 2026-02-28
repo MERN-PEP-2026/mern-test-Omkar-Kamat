560d5bead3648f441c9f693836f7deaade9927c1
Omkar Kamat <147087874+Omkar-Kamat@users.noreply.github.com>
2026-03-01 00:27:13 +0530

    deploy debugs

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173" || process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true
   })
 );
